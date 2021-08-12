5a544bef5e8bb8a2dc63df47b7efc0a221ac7dab
Hikaru Uchiyama <hikariru@users.noreply.github.com>
2021-08-12 19:42:18 +0900

    postgresql

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,6 @@
 module.exports = {
   "type": "postgres",
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASS,
-  "database": process.env.DB_NAME,
+  "url": process.env.DATABASE_URL,
   "ssl": {
     rejectUnauthorized: false
   },
